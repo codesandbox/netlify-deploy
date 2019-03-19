184f7fd2a22b4ba91982d03362d67ee6c379563d
Sara Vieira <hey@iamsaravieira.com>
2019-03-19 11:14:30 +0100

    add healthz

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const { router, get, post } = require('microrouter')
+const { send } = require('micro')
+const logger = require('./utils/logger')
 const microCors = require('micro-cors')
 const claimSite = require('./routes/claim')
 const getSite = require('./routes/getSite')
@@ -10,6 +12,10 @@ const cors = microCors()
 
 module.exports = cors(
   router(
+    get('/healthz', (req, res) => {
+      logger.log('info', 'All Good')
+      send(res, 200, { ok: true })
+    }),
     post('/site/:id/deploys', createDeploy),
     get('/site/:id/status', getStatus),
     get('/site/:id', getSite),
